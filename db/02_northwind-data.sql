632e5cedb112985debb226b24b82fc10fe3b221a
oonafoulser <89874292+oonafoulser@users.noreply.github.com>
2023-04-18 15:48:52 -0400

    Update 02_northwind-data.sql

--- db/02_northwind-data.sql
+++ db/02_northwind-data.sql
@@ -17,7 +17,7 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 
-USE `northwind`;
+USE `MunchApp`;
 
 #
 # Dumping data for table 'customers'
