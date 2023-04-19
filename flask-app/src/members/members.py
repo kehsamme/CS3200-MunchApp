0744d706eda36b5583778b7953130f8889bf8f90
emmashek <shek.em@northeastern.edu>
2023-04-18 20:36:11 -0400

    members post and get requests

--- flask-app/src/members/members.py
+++ flask-app/src/members/members.py
@@ -1,16 +1,47 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
 
 members = Blueprint('members', __name__)
 
-# Get all customers from the DB
-@members.route('/customers', methods=['GET'])
-def get_customers():
+# # Get all customers from the DB
+# @members.route('/customers', methods=['GET'])
+# def get_customers():
+#     cursor = db.get_db().cursor()
+#     cursor.execute('select company, last_name,\
+#         first_name, job_title, business_phone from customers')
+#     row_headers = [x[0] for x in cursor.description]
+#     json_data = []
+#     theData = cursor.fetchall()
+#     for row in theData:
+#         json_data.append(dict(zip(row_headers, row)))
+#     the_response = make_response(jsonify(json_data))
+#     the_response.status_code = 200
+#     the_response.mimetype = 'application/json'
+#     return the_response
+
+# # Get customer detail for customer with particular userID
+# @members.route('/customers/<userID>', methods=['GET'])
+# def get_customer(userID):
+#     cursor = db.get_db().cursor()
+#     cursor.execute('select * from customers where id = {0}'.format(userID))
+#     row_headers = [x[0] for x in cursor.description]
+#     json_data = []
+#     theData = cursor.fetchall()
+#     for row in theData:
+#         json_data.append(dict(zip(row_headers, row)))
+#     the_response = make_response(jsonify(json_data))
+#     the_response.status_code = 200
+#     the_response.mimetype = 'application/json'
+#     return the_response
+
+
+# Get location of restaurant based on city
+@members.route('/restaurants/<city>', methods=['GET'])
+def get_customer(city):
     cursor = db.get_db().cursor()
-    cursor.execute('select company, last_name,\
-        first_name, job_title, business_phone from customers')
+    cursor.execute('select ResName, City, Street, State, Zip from Restaurants where City = {city}'.format(city))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -21,17 +52,66 @@ def get_customers():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get customer detail for customer with particular userID
-@members.route('/customers/<userID>', methods=['GET'])
-def get_customer(userID):
+
+# Get a list of the top ten highest rated restaurants
+@members.route('/tenBestRated', methods=['GET'])
+def get_10_best_rated():
+
+    query = '''
+        SELECT ResName, Cuisine, PriceRange, City, Descriptions
+        FROM Restaurants
+        ORDER BY Rating
+        LIMIT 10
+    '''
+
     cursor = db.get_db().cursor()
-    cursor.execute('select * from customers where id = {0}'.format(userID))
-    row_headers = [x[0] for x in cursor.description]
+    cursor.execute(query)
+
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in 
+    # putting column headers together with data
     json_data = []
+
+    # fetch all the data from the cursor
     theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers. 
     for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
+# Post a photo of the food
+@members.route('/postPhoto', methods=['POST'])
+def post_new_photo():
+
+    # collecting the data from the request object
+    the_data = request.json
+    current_app.logger.info(the_data)
+    
+    # extracting the variable
+    r_photo = the_data['Photo']
+    r_id = the_data['ResID']
+    
+    # constructing the query
+    insert_statement = 'insert into Photos (Photo, ResID) values ("'
+    insert_statement += r_photo + '", "'
+    insert_statement += r_id + ')'
+    current_app.logger.info(insert_statement)
+    
+    # execute and commit the query
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_statement)
+    db.get_db(). commit()
+
+
+    return "success!"
+
+# Delete friends
+
+
+
+
--- flask-app/src/owners/owners.py
+++ flask-app/src/owners/owners.py
@@ -111,3 +111,107 @@ def get_competitor_list():
 # Update owner contact information
 
 
+# Post a photo of their restaurant
+@owners.route('/photos/<ResID>', methods =['POST'])
+
+def add_photo(ResID):
+
+    #access json data from request object
+    current_app.logger.info('')
+    req_data = request.get_json()
+    current_app.logger.info(req_data)
+    
+    #extracting the variable
+    photo = req_data['Photo']
+                     
+    #construct insert statement
+    
+    insert_statement = 'INSERT INTO Photos(ResID, Photo) VALUES ("'
+    insert_statement += ResID + '","' + photo + ')'
+                     
+    current_app.logger.info(insert_statement)
+                     
+    #execute query
+    cursor = db.get_db().cursor()
+    cursor.execute(insert_statement)
+    db.get_db().commit()
+    return "Success!"
+    
+#Get a list of customers who’ve been to the restaurant and their member info
+@owners.route('/members/<ResID>', methods =['GET'])
+def get_customers(ResID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select Email, FName, LName, Age, PhoneNumber, City, State, NumReviews,\
+                    MemberID from (Restaurants join ReviewPhotos on ResID = ResID) join\
+                    (Members join ReviewPhotos on MemberID = MemberID)'.format(ResID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+
+#Get a list of reviews at the restaurant.
+@owners.route('/retaurant/<ReviewDescription>/<ResID>', methods =['GET'])
+def get_reviews(ResID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select Stars, ReviewDescription, MemberID, ResID\
+                 from (Restaurants join Reviews on ResID = ResID)'.format(ResID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+#List of rewards offered by the restaurant
+@owners.route('/rewards/<ResID>', methods =['GET'])
+def get_rewards(ResID):
+    cursor = db.get_db().cursor()
+    cursor.execute('select RewardType, PointsRequired, DatesOffered, ResID\
+                 from (Restaurants join Rewards on ResID = ResID)'.format(ResID))
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# # Add menu and menu prices
+# @owners.route('/restaurants', methods =['POST'])
+# def add_menu_items():
+    
+#     #accsess json data from request object
+#     current_app.logger.info('')
+#     req_data = request.get_json()
+#     current_app.logger.info(req_data)
+    
+#     res_id = req_data['ResID']
+#     dish_name = req_data['DishName']
+#     meal_type = req_data['MealType']
+#     price = req_data['Price']
+                     
+#     #construct insert statement
+    
+#     insert_statement = 'INSERT INTO Dishes(DishName, MealType, Price) VALUES ("'
+#     insert_statement += res_id + '","' + dish_name + '","' + meal_type + '","' + str(price) + ')'
+                     
+#     current_app.logger.info(insert_statement)
+                     
+#     #execute query
+#     cursor = db.get_db().cursor()
+#     cursor.execute(insert_statement)
+#     db.get_db().commit()
+#     return "Success"
+    
+
