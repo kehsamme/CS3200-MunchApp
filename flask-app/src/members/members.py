360d307ee056f83bbdeb68e46caf209aa9f891f9
emmashek <shek.em@northeastern.edu>
2023-04-20 02:13:59 -0400

    clean members.py

--- flask-app/src/members/members.py
+++ flask-app/src/members/members.py
@@ -5,37 +5,6 @@ from src import db
 
 members = Blueprint('members', __name__)
 
-# # Get all customers from the DB
-# @members.route('/customers', methods=['GET'])
-# def get_customers():
-#     cursor = db.get_db().cursor()
-#     cursor.execute('select company, last_name,\
-#         first_name, job_title, business_phone from customers')
-#     row_headers = [x[0] for x in cursor.description]
-#     json_data = []
-#     theData = cursor.fetchall()
-#     for row in theData:
-#         json_data.append(dict(zip(row_headers, row)))
-#     the_response = make_response(jsonify(json_data))
-#     the_response.status_code = 200
-#     the_response.mimetype = 'application/json'
-#     return the_response
-
-# # Get customer detail for customer with particular userID
-# @members.route('/customers/<userID>', methods=['GET'])
-# def get_customer(userID):
-#     cursor = db.get_db().cursor()
-#     cursor.execute('select * from customers where id = {0}'.format(userID))
-#     row_headers = [x[0] for x in cursor.description]
-#     json_data = []
-#     theData = cursor.fetchall()
-#     for row in theData:
-#         json_data.append(dict(zip(row_headers, row)))
-#     the_response = make_response(jsonify(json_data))
-#     the_response.status_code = 200
-#     the_response.mimetype = 'application/json'
-#     return the_response
-
 
 # Get location of restaurant based on city
 @members.route('/restaurants/<City>', methods=['GET'])
@@ -175,11 +144,6 @@ def add_rating():
     query += str(res_id) + ')'
 
     current_app.logger.info(query)
-
-    # # constructing the query
-    # query = 'insert into Restaurants (Rating) values("'
-    # query += str(r_rating) + ')'
-    # current_app.logger.info(query)
 
     # execute and commit the query
     cursor = db.get_db().cursor()
